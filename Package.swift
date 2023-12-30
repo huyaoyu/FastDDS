bd4ebdc7e71bc24bf148a20df48862e33a046de3
Yaoyu Hu <yaoyuh@andrew.cmu.edu>
2023-12-29 23:51:25 -0500

    Build v2.6.6

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "FastDDS",
-                      url: "https://github.com/huyaoyu/FastDDSPrebuild/releases/download/v2.6.6/fastrtps-v2.6.6.xcframework.zip",
-                      checksum: "97f0248e9197e9aed0e69bb974d266e38153f0d6fd9c050ef6709956c1e53208")
+                      url: "https://github.com/huyaoyu/FastDDS/releases/download/v2.6.6/fastrtps-v2.6.6.xcframework.zip",
+                      checksum: "e91a3462982202ea62b40d4afffb126f350b150e712fa230c5562112becdceb7")
     ]
 )
